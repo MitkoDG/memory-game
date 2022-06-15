79147d7f0160815757b814a102bb8a45282dad6d
DimitarDG <83611831+MitkoDG@users.noreply.github.com>
2022-06-15 17:06:21 +0300

    fixing bugs
    finished

--- app.js
+++ app.js
@@ -3,95 +3,95 @@ document.addEventListener('DOMContentLoaded', () => {
     const cardsList = [
         {
             name: 'cat',
-            image: './images/cat.png'
+            image: 'images/cat.png'
         },
         {
             name: 'cat',
-            image: './images/cat.png'
+            image: 'images/cat.png'
         },
         {
             name: 'dice',
-            image: './images/dice.png'
+            image: 'images/dice.png'
         },
         {
             name: 'dice',
-            image: './images/dice.png'
+            image: 'images/dice.png'
         },
         {
             name: 'dog',
-            image: './images/dog.png'
+            image: 'images/dog.png'
         },
         {
             name: 'dog',
-            image: './images/dog.png'
+            image: 'images/dog.png'
         },
         {
             name: 'kids',
-            image: './images/kids.png'
+            image: 'images/kids.png'
         },
         {
             name: 'kids',
-            image: './images/kids.png'
+            image: 'images/kids.png'
         },
         {
             name: 'mickey',
-            image: './images/mickey.png'
+            image: 'images/mickey.png'
         },
         {
             name: 'mickey',
-            image: './images/mickey.png'
+            image: 'images/mickey.png'
         },
         {
             name: 'penguin',
-            image: './images/penguin.png'
+            image: 'images/penguin.png'
         },
         {
             name: 'penguin',
-            image: './images/penguin.png'
+            image: 'images/penguin.png'
         },
         {
             name: 'simpsons',
-            image: './images/simpsons.png'
+            image: 'images/simpsons.png'
         },
         {
             name: 'simpsons',
-            image: './images/simpsons.png'
+            image: 'images/simpsons.png'
         },
         {
             name: 'sponge',
-            image: './images/sponge.png'
+            image: 'images/sponge.png'
         },
         {
             name: 'sponge',
-            image: './images/sponge.png'
+            image: 'images/sponge.png'
         },
         {
             name: 'yoshi',
-            image: './images/yoshi.png'
+            image: 'images/yoshi.png'
         },
         {
             name: 'yoshi',
-            image: './images/yoshi.png'
+            image: 'images/yoshi.png'
         },
         {
             name: 'mario',
-            image: './images/mario.png'
+            image: 'images/mario.png'
         },
         {
             name: 'mario',
-            image: './images/mario.png'
+            image: 'images/mario.png'
         },
         // {
         //     name: 'dora',
-        //     image: './images/dora.png'
+        //     image: 'images/dora.png'
         // },
         // {
         //     name: 'dora',
-        //     image: './images/dora.png'
+        //     image: 'images/dora.png'
         // },
         // {
         //     name: 'blank',
-        //     image: './images/blank.png'
+        //     image: 'images/blank.png'
         // }
     ];
     
@@ -99,8 +99,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.gameGrid');
     const attemptHolder = document.querySelector('.attemptsHolder')
     const foundHolder = document.querySelector('.foundHolder');
-
     let cardsInGame = 10;
+
     let attempts = 0;
     let foundCards = 0;
     attemptHolder.textContent = attempts;
@@ -112,7 +112,7 @@ document.addEventListener('DOMContentLoaded', () => {
     function initiateBoard() {
         for (let i = 0; i < cardsList.length; i++) {
             let card = document.createElement('img');
-            card.setAttribute('src', './images/blank.png');
+            card.setAttribute('src', 'images/javascript.png'); // or logo
             card.setAttribute('data-id', i); // i is the id from the loop
             card.addEventListener('click', flipCard);
             grid.appendChild(card);
@@ -122,11 +122,13 @@ document.addEventListener('DOMContentLoaded', () => {
     function flipCard() {
         if (chosenCards.length != 2) {
             let cardId = this.getAttribute('data-id');
-            if (this.getAttribute('src') != './images/blank.png') {
+            if (this.getAttribute('src') != 'images/check.png') {
                 chosenCards.push(cardsList[cardId].name);
                 chosenCardsIds.push(cardId);
                 this.setAttribute('src', cardsList[cardId].image);
-                if (chosenCards.lenght == 2) {
+                console.log(chosenCards);
+                console.log(cardsList[cardId].image);
+                if (chosenCards.length == 2) {
                     setTimeout(checkForMatch, 400);
                 }
             }
@@ -135,23 +137,26 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function checkForMatch() {
+        console.log('check');
         attempts++;
         let cards = document.querySelectorAll('img');
         let firstCard = chosenCardsIds[0];
         let secondCard = chosenCardsIds[1];
+        console.log(chosenCards);
+
         if (chosenCards[0] == chosenCards[1]) {
             foundCards++;
-            cards[firstCard].setAttribute('src', './images/blank.png')
-            cards[secondCard].setAttribute('src', './images/blank.png')
+            cards[firstCard].setAttribute('src', 'images/check.png')
+            cards[secondCard].setAttribute('src', 'images/check.png')
         } else {
-            cards[firstCard].setAttribute('src', './images/plaseholder.png')
-            cards[secondCard].setAttribute('src', './images/plaseholder.png')
+            cards[firstCard].setAttribute('src', 'images/javascript.png')
+            cards[secondCard].setAttribute('src', 'images/javascript.png')
         }
         chosenCards = [];
         chosenCardsIds = [];
         attemptHolder.textContent = attempts;
         foundHolder.textContent = foundCards;
-
+        console.log(foundCards);
         if (foundCards == cardsInGame) {
             alert('Well done !')
         }
