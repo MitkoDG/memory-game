f2dedffaa433cd38459f17f7f1076bb5b095b5d6
DimitarDG <83611831+MitkoDG@users.noreply.github.com>
2022-06-15 15:02:09 +0300

    it should work but its not
    :)

--- src/app.js
+++ src/app.js
@@ -3,95 +3,95 @@ document.addEventListener('DOMContentLoaded', () => {
     const cardsList = [
         {
             name: 'cat',
-            image: '../images/cat.png'
+            image: './images/cat.png'
         },
         {
             name: 'cat',
-            image: '../images/cat.png'
+            image: './images/cat.png'
         },
         {
             name: 'dice',
-            image: '../images/dice.png'
+            image: './images/dice.png'
         },
         {
             name: 'dice',
-            image: '../images/dice.png'
+            image: './images/dice.png'
         },
         {
             name: 'dog',
-            image: '../images/dog.png'
+            image: './images/dog.png'
         },
         {
             name: 'dog',
-            image: '../images/dog.png'
+            image: './images/dog.png'
         },
         {
             name: 'kids',
-            image: '../images/kids.png'
+            image: './images/kids.png'
         },
         {
             name: 'kids',
-            image: '../images/kids.png'
+            image: './images/kids.png'
         },
         {
             name: 'mickey',
-            image: '../images/mickey.png'
+            image: './images/mickey.png'
         },
         {
             name: 'mickey',
-            image: '../images/mickey.png'
+            image: './images/mickey.png'
         },
         {
             name: 'penguin',
-            image: '../images/penguin.png'
+            image: './images/penguin.png'
         },
         {
             name: 'penguin',
-            image: '../images/penguin.png'
+            image: './images/penguin.png'
         },
         {
             name: 'simpsons',
-            image: '../images/simpsons.png'
+            image: './images/simpsons.png'
         },
         {
             name: 'simpsons',
-            image: '../images/simpsons.png'
+            image: './images/simpsons.png'
         },
         {
             name: 'sponge',
-            image: '../images/sponge.png'
+            image: './images/sponge.png'
         },
         {
             name: 'sponge',
-            image: '../images/sponge.png'
+            image: './images/sponge.png'
         },
         {
             name: 'yoshi',
-            image: '../images/yoshi.png'
+            image: './images/yoshi.png'
         },
         {
             name: 'yoshi',
-            image: '../images/yoshi.png'
+            image: './images/yoshi.png'
         },
         {
             name: 'mario',
-            image: '../images/mario.png'
+            image: './images/mario.png'
         },
         {
             name: 'mario',
-            image: '../images/mario.png'
+            image: './images/mario.png'
         },
         // {
         //     name: 'dora',
-        //     image: '../images/dora.png'
+        //     image: './images/dora.png'
         // },
         // {
         //     name: 'dora',
-        //     image: '../images/dora.png'
+        //     image: './images/dora.png'
         // },
         // {
         //     name: 'blank',
-        //     image: '../images/blank.png'
+        //     image: './images/blank.png'
         // }
     ];
     
@@ -112,7 +112,7 @@ document.addEventListener('DOMContentLoaded', () => {
     function initiateBoard() {
         for (let i = 0; i < cardsList.length; i++) {
             let card = document.createElement('img');
-            card.setAttribute('src', '../images/blank.png');
+            card.setAttribute('src', './images/blank.png');
             card.setAttribute('data-id', i); // i is the id from the loop
             card.addEventListener('click', flipCard);
             grid.appendChild(card);
@@ -120,10 +120,9 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function flipCard() {
-        console.log('flipFunc');
         if (chosenCards.length != 2) {
             let cardId = this.getAttribute('data-id');
-            if (this.getAttribute('src') != '../images/blank.png') {
+            if (this.getAttribute('src') != './images/blank.png') {
                 chosenCards.push(cardsList[cardId].name);
                 chosenCardsIds.push(cardId);
                 this.setAttribute('src', cardsList[cardId].image);
@@ -142,8 +141,11 @@ document.addEventListener('DOMContentLoaded', () => {
         let secondCard = chosenCardsIds[1];
         if (chosenCards[0] == chosenCards[1]) {
             foundCards++;
-            cards[firstCard].setAttribute('src', '../images/blank.png')
-            cards[secondCard].setAttribute('src', '../images/blank.png')
+            cards[firstCard].setAttribute('src', './images/blank.png')
+            cards[secondCard].setAttribute('src', './images/blank.png')
+        } else {
+            cards[firstCard].setAttribute('src', './images/plaseholder.png')
+            cards[secondCard].setAttribute('src', './images/plaseholder.png')
         }
         chosenCards = [];
         chosenCardsIds = [];
