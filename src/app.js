35494f517c607430243b3141cb0b29e403e9513b
DimitarDG <83611831+MitkoDG@users.noreply.github.com>
2022-06-15 11:54:11 +0300

    initial commit
    added index and link the script and css file

---
+++ src/app.js
@@ -0,0 +1,99 @@
+document.addEventListener('DOMContentLoaded', () =>{
+
+    const cardsList = [
+        {
+            name: 'cat',
+            image: '../images/cat.png'
+        },
+        {
+            name: 'cat',
+            image: '../images/cat.png'
+        },
+        {
+            name: 'dice',
+            image: '../images/dice.png'
+        },
+        {
+            name: 'dice',
+            image: '../images/dice.png'
+        },
+        {
+            name: 'dog',
+            image: '../images/dog.png'
+        },
+        {
+            name: 'dog',
+            image: '../images/dog.png'
+        },
+        {
+            name: 'kids',
+            image: '../images/kids.png'
+        },
+        {
+            name: 'kids',
+            image: '../images/kids.png'
+        },
+        {
+            name: 'mickey',
+            image: '../images/mickey.png'
+        },
+        {
+            name: 'mickey',
+            image: '../images/mickey.png'
+        },
+        {
+            name: 'penguin',
+            image: '../images/penguin.png'
+        },
+        {
+            name: 'penguin',
+            image: '../images/penguin.png'
+        },
+        {
+            name: 'simpsons',
+            image: '../images/simpsons.png'
+        },
+        {
+            name: 'simpsons',
+            image: '../images/simpsons.png'
+        },
+        {
+            name: 'sponge',
+            image: '../images/sponge.png'
+        },
+        {
+            name: 'sponge',
+            image: '../images/sponge.png'
+        },
+        {
+            name: 'yoshi',
+            image: '../images/yoshi.png'
+        },
+        {
+            name: 'yoshi',
+            image: '../images/yoshi.png'
+        },
+        {
+            name: 'mario',
+            image: '../images/mario.png'
+        },
+        {
+            name: 'mario',
+            image: '../images/mario.png'
+        },
+        {
+            name: 'dora',
+            image: '../images/dora.png'
+        },
+        {
+            name: 'dora',
+            image: '../images/dora.png'
+        },
+        {
+            name: 'blank',
+            image: '../images/blank.png'
+        }
+    ]
+
+
+})
